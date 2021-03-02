1af64f4e6e91105e9389dd4d3de784d51fd567e5
juuliana <jumaju10@hotmail.com>
2021-03-02 19:00:51 -0300

    UserController --updated

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController{
     async create(request: Request, response: Response){
         const { name, email } = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -25,6 +25,14 @@ class UserController{
         await usersRepository.save(user);
 
         return response.json(user);
+    }
+
+    async show(request: Request, response: Response){
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const all = await usersRepository.find();
+
+        return response.json(all);
     }
 }
 
