d05eebe4edff69c8f29376dbb395b1d357ff9f5f
juuliana <jumaju10@hotmail.com>
2021-03-06 20:25:09 -0300

    SendMailController --

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/sendMailService";
+
+class SendMailController{
+
+    async execute(request: Request, response: Response){
+        const { email, survey_id } = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+        
+        const userAlreadyExists = await usersRepository.findOne({email});
+
+        if(!userAlreadyExists){
+            return response.status(400).json({
+                error: "User does not exists",
+            });
+        }
+
+        const survey = await surveysRepository.findOne({id: survey_id});
+
+        if(!survey){
+            return response.status(400).json({
+                error: "Survey does not exists!"
+            });
+        }
+
+        //Salvar as informações na tqabela surveyUser
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id
+        })
+
+        await surveysUsersRepository.save(surveyUser);
+
+        //Enviar e-mail para o usuário
+        await SendMailService.execute(email, survey.title, survey.description);
+
+        return response.json(surveyUser);
+    }
+}
+
+export { SendMailController }
