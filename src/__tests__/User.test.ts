3bf68255aa4399a20a42b96d0ab21c0f52a0c1bb
= <=>
2021-03-06 15:32:56 -0300

    User.test

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,21 +1,36 @@
-import request from 'supertest';
-import { app } from '../app';
+import request from "supertest";
+import { getConnection } from "typeorm";
+import { app } from "../app";
 
-import createConnection from '../database';
+import createConnection from "../database";
 
 describe("Users", () => {
-    beforeAll(async() => {
-        const connection = await createConnection();
-        await connection.runMigrations();
-    })
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
 
-    it("Should be able to create a new user", async () => {
-        const response = await request(app).post("/users")
-        .send({
-            email: "user@example.com",
-            nome: "User example"
-        });
-        
-        expect(response.status).toBe(201);
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: "User Example",
     });
-})
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a user with exists email", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: "User Example",
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
