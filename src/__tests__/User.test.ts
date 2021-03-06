43df9f11034d12f08ae44896623e2a4492d7c72d
= <=>
2021-03-05 22:39:46 -0300

    testes no sistema

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,11 +1,21 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe("User", () => {
+import createConnection from '../database';
 
-    request(app).post("/users")
-    .send({
-        email: "user@example.com",
-        nome: "User example"
+describe("Users", () => {
+    beforeAll(async() => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    })
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users")
+        .send({
+            email: "user@example.com",
+            nome: "User example"
+        });
+        
+        expect(response.status).toBe(201);
     });
 })
