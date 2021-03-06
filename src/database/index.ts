c791fcbf44ae09825ea92b899ba67c501a47a373
= <=>
2021-03-05 22:38:52 -0300

    update database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,13 @@
-import {  Connection, createConnection } from "typeorm";
+import {  Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
-    return createConnection();
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database: process.env.NODE_ENV === 'test'
+                        ? "./src/database/database.test.sqlite"
+                        : defaultOptions.database
+        })
+    );
 }
